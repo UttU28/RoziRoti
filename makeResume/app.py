8b8a53b15cbf409a1b83540e7ab96f61d5d57f6f
UttU28 <utsavmaan28@gmail.com>
2024-05-29 14:40:23 -0500

    JSK

--- makeResume/app.py
+++ makeResume/app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
+from flask_weasyprint import HTML
 import json
+import os
 
+
+allData = 'utsavChaudhary.json'
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    with open('utsavChaudhary.json', 'r') as json_file:
+def index_pdf():
+    with open(allData, 'r') as json_file:
         data = json.load(json_file)
-    return render_template('index.html', data=data)
+    html = render_template('index.html', data=data)
+    pdf = HTML(string=html).write_pdf()
+
+    pdf_path = os.path.join('static', 'Utsav_Chaudhary_Resume.pdf')
+    with open(pdf_path, 'wb') as f:
+        f.write(pdf)
+
+    return html
+    # return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
